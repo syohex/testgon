1f5a5ced258b761b1ae8c4138118b1ecba871d50
Syohei YOSHIDA <syohex@gmail.com>
2014-06-09 15:29:54 +0900

    Add signed min value test

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -15,3 +15,20 @@ func TestSignedMaxValue(t *testing.T) {
 		t.Errorf(`Expected: "2147483647L" but got %s`, signedLongMax)
 	}
 }
+
+func TestSignedMinValue(t *testing.T) {
+	signedShortMin := signedMinValue(`short`, 16, 2)
+	if signedShortMin != "-32768" {
+		t.Errorf(`Expected: "-32768" but got %s`, signedShortMin)
+	}
+
+	signedShortMin2 := signedMinValue(`short`, 16, 1)
+	if signedShortMin2 != "-32767" {
+		t.Errorf(`Expected: "3-2767" but got %s`, signedShortMin2)
+	}
+
+	signedLongLongMin := signedMinValue(`long long`, 8, 2)
+	if signedLongLongMin != "-128LL" {
+		t.Errorf(`Expected: "128" but got %s`, signedLongLongMin)
+	}
+}
