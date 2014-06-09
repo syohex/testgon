c85193f79dd86bf2f59d8f24b742b2c7057ec4dd
Syohei YOSHIDA <syohex@gmail.com>
2014-06-09 13:34:21 +0900

    implement set predefined macros

--- generator/generator.go
+++ generator/generator.go
@@ -8,24 +8,25 @@ import (
 	"fmt"
 	"regexp"
 	"os"
+	"strings"
 	"math"
 )
 
 type Param struct {
 	File string
-	Help boolean
-	IntOnly boolean
-	FloatOnly boolean
+	Help bool
+	IntOnly bool
+	FloatOnly bool
 }
 
 type Generator struct {
-	Config config.Config
-	Help boolean
-	IntOnly boolean
-	FloatOnly boolean
+	Config *config.Config
+	Help bool
+	IntOnly bool
+	FloatOnly bool
 }
 
-func New(param Config) (Generator, error){
+func New(param Param) (*Generator, error){
 	conf, err := config.Parse(param.File)
 	if err != nil {
 		return nil, err
@@ -35,24 +36,24 @@ func New(param Config) (Generator, error){
 		Config: conf,
 		Help: param.Help,
 		IntOnly: param.IntOnly,
-		FloatOnly: param.FloatOnly
+		FloatOnly: param.FloatOnly,
 	}
 
 	return generator, nil
 }
 
 func expandPatterns(patterns []string) ([]string, error) {
-	files = []string
+	files := make([]string, 0)
 	for _, pattern := range patterns {
 		expands, err := filepath.Glob(pattern)
 		if err != nil {
-			return fmt.Errorf("Can't expand '%s'", pattern)
+			return nil, fmt.Errorf("Can't expand '%s'", pattern)
 		}
 
-		files = append(files, ...expands)
+		files = append(files, expands...)
 	}
 
-	return files
+	return files, nil
 }
 
 var templateFileSuffix = regexp.MustCompile(`\.tt$`)
@@ -67,14 +68,16 @@ func checkTemplateFileName(templates []string) error {
 	return nil
 }
 
-func (generator *Generator)generateTestSuite(templates []string) {
-	if err := os.Mkdir(generator.Config.TestDir); err != nil {
+func (generator *Generator)generateTestSuite(templates []string) error {
+	if err := os.Mkdir(generator.Config.TestDir, 0755); err != nil {
 		return err
 	}
 
-	for _, template := range templates {
-		// generate template file
-	}
+//	for _, template := range templates {
+//		// generate template file
+//	}
+
+	return nil
 }
 
 func (generator *Generator)Run(patterns []string) error {
@@ -93,88 +96,100 @@ func (generator *Generator)Run(patterns []string) error {
 
 	// TODO set macros
 
-	generator.generaateTestSuite(templates)
+	if err := generator.generateTestSuite(templates); err != nil {
+		return err
+	}
+
+	return nil
 }
 
 var spaceRe = regexp.MustCompile(`\s+`)
 
-func setPredefinedMacros(conf config.Config) {
-	typeSuffix := map[string]string {
-		"long": "L",
-		"long long": "LL",
-		"float": "F",
-		"long double": "L",
-	}
-
+func setPredefinedMacros(conf config.Config) map[string]*macro.Macro {
 	predefined := make(map[string]*macro.Macro)
-	for typeName, size := range conf.Size {
-		typeName = spaceRe.ReplaceAllString(typeName, "")
+	registerTypeMacros(predefined, "char", conf.Size.Char, conf.Complement)
+	registerTypeMacros(predefined, "short", conf.Size.Short, conf.Complement)
+	registerTypeMacros(predefined, "int", conf.Size.Int, conf.Complement)
+	registerTypeMacros(predefined, "long", conf.Size.Long, conf.Complement)
+	// should implement pointer type and 'float' and 'double'
+
+	return predefined
+}
+
+func typeSuffix(typeName string) string {
+	switch typeName {
+	case "long":
+		return "L"
+	case "long long":
+		return "LL"
+	case "float":
+		return "F"
+	case "long double":
+		return "L"
+	default:
+		return ""
 	}
 }
 
-type Limit struct {
-	signedCharMin int64
-	signedCharMax int64
-	unsighedCharMin uint64
-	unsignedCharMax uint64
-
-	singedShortMin int64
-	singedShortMax int64
-	unsignedShortMin int64
-	unsignedShortMax int64
-
-	signedIntMin int64
-	signedIntMax int64
-	unsignedIntMin int64
-	unsignedIntMax int64
-
-	signedLongMin int64
-	singedLongMax int64
-	unsignedLongMin int64
-	unsignedLongMax int64
-
-	signedLongLongMin int64
-	signedLongLongMax int64
-	unsignedLongLongMin int64
-	unsignedLongLongMax int64
+func signedMaxValue(typeName string, bitWidth int) string {
+	suffix := typeSuffix(typeName);
+	width := float64(bitWidth)
+	return fmt.Sprintf("%g%s", math.Pow(2, (width - 1)) - 1, suffix)
 }
 
-func getTypeLimit(size config.Size, complement int) *Limit {
-	limit := &Limit{}
+func signedMinValue(typeName string, bitWidth int, complement int) string {
+	suffix := typeSuffix(typeName);
 
-	// Use string and fmt.Sprintf for representing limit number
-	for typeName, bitWidth := range size {
-		switch typeName {
-		case "char":
-			limit.signedCharMin = -(2 ** (bitWidth - 1))
-			limit.signedCharMax = (2 ** (bitWidth - 1)) - 1
-			limit.unsignedCharMax = (2 ** bitWidth) - 1
-		case "short":
-			limit.signedShortMin = -(2 ** (bitWidth - 1))
-			limit.signedShortMax = (2 ** (bitWidth - 1)) - 1
-			limit.unsignedShortMax = (2 ** bitWidth) - 1
-		case "int":
-			limit.signedIntMin = -(2 ** (bitWidth - 1))
-			limit.signedIntMax = (2 ** (bitWidth - 1)) - 1
-			limit.unsignedIntMax = (2 ** bitWidth) - 1
-		case "long":
-			limit.signedLongMin = -(2 ** (bitWidth - 1))
-			limit.signedLongMax = (2 ** (bitWidth - 1)) - 1
-			limit.unsignedLongMax = (2 ** bitWidth) - 1
-		case "longlong":
-			limit.signedLongLongMin = -(2 ** (bitWidth - 1))
-			limit.signedLongLongMax = (2 ** (bitWidth - 1)) - 1
-			limit.unsignedLongLongMax = (2 ** bitWidth) - 1
-		}
+	width := float64(bitWidth)
+	if complement == 2 {
+		return fmt.Sprintf("%g%s", -math.Pow(2, (width - 1)), suffix)
+	} else {
+		return fmt.Sprintf("%g%s", -math.Pow(2 , (width - 1)) + 1, suffix)
+	}
+}
+
+func unsignedMaxValue(typeName string, bitWidth int) string {
+	suffix := typeSuffix(typeName);
+	width := float64(bitWidth)
+	return fmt.Sprintf("%g%s", math.Pow(2, width) - 1, suffix)
+}
+
+// use Enum type instead of bool type ???
+func macroTypeName(typeName string, unsigned bool, min bool) string {
+	var unsignedPrefix string
+	if unsigned {
+		unsignedPrefix = "U"
+	} else {
+		unsignedPrefix = ""
 	}
 
-	if complement == 1 {
-		limit.signedCharMin = -(2 ** (bitWidth - 1))
-		limit.signedShortMin = -(2 ** (bitWidth - 1))
-		limit.signedIntMin = -(2 ** (bitWidth - 1))
-		limit.signedLongMin = -(2 ** (bitWidth - 1))
-		limit.signedLongLongMin = -(2 ** (bitWidth - 1))
+	var suffix string
+	if min {
+		suffix = "MIN"
+	} else {
+		suffix = "MAX"
 	}
 
-	return limit
+	return fmt.Sprintf("%s%s%s", unsignedPrefix, strings.ToUpper(typeName), suffix)
+}
+
+func registerTypeMacros(
+	predefined map[string]*macro.Macro,
+	typeName string,
+	bitWidth int,
+	complement int,
+) {
+	signedMin := signedMinValue(typeName, bitWidth, complement)
+	signedMax := signedMaxValue(typeName, bitWidth)
+	unsignedMax := unsignedMaxValue(typeName, bitWidth)
+
+	signedMinName := macroTypeName(typeName, false, true)
+	signedMaxName := macroTypeName(typeName, false, false)
+	unsignedMinName := macroTypeName(typeName, true, true)
+	unsignedMaxName := macroTypeName(typeName, true, false)
+
+	predefined[signedMinName] = &macro.Macro{Name: signedMinName, Body: signedMin}
+	predefined[signedMaxName] = &macro.Macro{Name: signedMinName, Body: signedMax}
+	predefined[unsignedMinName] = &macro.Macro{Name: signedMinName, Body: "0"}
+	predefined[unsignedMaxName] = &macro.Macro{Name: signedMinName, Body: unsignedMax}
 }
