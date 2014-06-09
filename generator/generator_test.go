8493de544cc51ae1b2dd9860ee76a863d7f4bc3a
Syohei YOSHIDA <syohex@gmail.com>
2014-06-09 14:30:19 +0900

    Add signed 'long' max test

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -7,6 +7,11 @@ import (
 func TestSignedMaxValue(t *testing.T) {
 	signedCharMax := signedMaxValue(`char`, 8)
 	if signedCharMax != "127" {
-		t.Errorf("Expected: 127 but got %s", signedCharMax)
+		t.Errorf(`Expected: "127" but got %s`, signedCharMax)
+	}
+
+	signedLongMax := signedMaxValue(`long`, 32)
+	if signedLongMax != "2147483647L" {
+		t.Errorf(`Expected: "2147483647L" but got %s`, signedLongMax)
 	}
 }
